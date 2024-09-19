b07ae29551c817812ccc6510e405e5de99d02ab6
John H <hxjwork1@gmail.com>
2024-09-19 19:34:44 +0400

    Initialize logger

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    initialize_logger("config/log4rs.yaml")?;
+
+    Ok(())
+}
+
+fn initialize_logger(config_file: &str) -> anyhow::Result<()> {
+    log4rs::init_file(config_file, Default::default())?;
+    log::debug!("Log4rs successfully initialized");
+    Ok(())
 }
