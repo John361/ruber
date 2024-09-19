0e78102174efc2bf5583a4b2f1dc11b23522ad0a
John H <hxjwork1@gmail.com>
2024-09-19 19:36:23 +0400

    Wrap Route in Routes

--- src/lib/routing/routes.rs
+++ src/lib/routing/routes.rs
@@ -3,6 +3,11 @@ use serde::Deserialize;
 use crate::routing::Watch;
 
 #[derive(Debug, Deserialize)]
+pub struct Routes {
+    pub routes: Vec<Route>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Route {
     pub source: Watch,
     pub destination: Vec<Watch>,
